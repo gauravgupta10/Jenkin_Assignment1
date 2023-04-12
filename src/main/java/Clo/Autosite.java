2920da819f31254008369549f1c86686986b4a8c
gaurav <gaurav.gupta7@tothenew.com>
2023-04-12 16:08:03 +0530

    new

--- src/main/java/Clo/Autosite.java
+++ src/main/java/Clo/Autosite.java
@@ -1,52 +1,49 @@
 package Clo;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
-public class Autosite {
-	private static WebDriver driver;
+
+public class Autosite{
+
+private static WebDriver driver;
+
 	@BeforeMethod
-	public void browselaunch()
+	public void lauch() throws InterruptedException
 	{
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Gauran Gupta\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		driver=new ChromeDriver();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		 driver=new ChromeDriver();
+		 driver.get("https://mathsolver.microsoft.com/en");
+		  driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		  
 	}
-//
-	@Test
-	public void logincheck()
-	{
+       
+		@Test
+		public void Home() throws InterruptedException 
+		{
+			
+			driver.findElement(By.xpath("//button[@aria-label=\"7\"]")).click();
+			driver.findElement(By.xpath("//button[@aria-label=\"Add\"]")).click();
+			driver.findElement(By.xpath("//button[@aria-label=\"7\"]")).click();
+			Thread.sleep(4000);
+			System.out.println(driver.findElement(By.xpath("//div[@class=\"GuppyField_ia__128DY\"]")).getText());
+			
+			
+			
+		}
 		
-	driver.findElement(By.xpath("//div[@class=\"orangehrm-login-branding\"]")).isDisplayed();
-	driver.findElement(By.xpath("//input[@placeholder=\"Username\"]")).isDisplayed();
-	driver.findElement(By.xpath("//input[@placeholder=\"Password\"]")).isDisplayed();
-		
-	}
-
 	@AfterMethod
-	public void closeBrowser()
+	public void closebrowser()
 	{
-		//driver.close();
+		driver.quit();
 	}
-	
 }
